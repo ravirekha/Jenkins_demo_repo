e0f5f3f5c50a81ab29ed39a19afdcc370f3782a3
ravirekha <43343078+ravirekha@users.noreply.github.com>
2024-02-07 06:55:39 +0530

    Update example.sh

--- example.sh
+++ example.sh
@@ -3,5 +3,5 @@ if [ "$BUILD_STATUS" = "SUCCESS" ]; then
   # Add additional commands here
   
 else
-  echo "Build failed, skipping additional steps."
+  echo "Build failed, skipping additional steps !!."
 fi
